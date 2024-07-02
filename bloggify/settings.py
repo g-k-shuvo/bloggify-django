33d7a6885289ed1ea26391dc2a68016a6149c99f
Golam Kibria <45451363+g-k-shuvo@users.noreply.github.com>
2024-07-02 17:18:17 +0600

    Update settings.py

--- bloggify/settings.py
+++ bloggify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@-=*^ii-g#lph@!(u0%jt^8$nl2&bl)$5kvl2dzx^j&#=rp!z9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bloggify-django.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-db920.up.railway.app', '127.0.0.1']
 
 
 # Application definition
